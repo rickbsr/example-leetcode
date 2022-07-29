b587f287b2236a41375778332918e6b92055e74c
Rick <rick8220602@gmail.com>
2022-07-29 21:03:22 +0800

    Refactor

--- Qes_136_Single Number/README.md
+++
@@ -1,5 +0,0 @@
-# 136 Single Number
-
----
-
-testing
--- leetcode-code-sample/src/com/rick/problems/easy/SingleNumber.java
+++ leetcode-code-sample/src/com/rick/problems/easy/SingleNumber.java
@@ -8,18 +8,28 @@ public class SingleNumber {
 
     public static void main(String[] args) {
         int[] nums = {4, 1, 3, 3, 2, 1, 2};
-        int res = new SingleNumber().singleNumberXor(nums);
+        int res;
+
+        // 額外空間法
+        res = new SingleNumber().singleNumberContainer(nums);
+
+        // 排序法
+        res = new SingleNumber().singleNumberSort(nums);
+
+        // By X-Or
+        res = new SingleNumber().singleNumberXor(nums);
+
         System.out.println(res);
     }
 
-    public int singleNumberMap(int[] nums) {
+    public int singleNumberContainer(int[] nums) {
         Map<Integer, Boolean> map = new HashMap<>();
         for (int i : nums)
             if (map.put(i, true) != null) map.remove(i);
         return map.keySet().iterator().next();
     }
 
-    public int singleNumberSoft(int[] nums) {
+    public int singleNumberSort(int[] nums) {
         Arrays.sort(nums);
         if (nums.length == 1) return nums[0];
         for (int i = 0; i < nums.length - 1; i += 2)
