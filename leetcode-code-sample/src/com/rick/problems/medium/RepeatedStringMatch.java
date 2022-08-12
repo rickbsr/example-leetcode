bc9935053b3283fe59b1870f140f5693ea3a36ee
rickren <rickren@megabank.com.tw>
2022-08-12 14:01:09 +0800

    0686

--- leetcode-code-sample/src/com/rick/problems/medium/RepeatedStringMatch.java
+++ leetcode-code-sample/src/com/rick/problems/medium/RepeatedStringMatch.java
@@ -4,12 +4,12 @@ public class RepeatedStringMatch {
 
     public static void main(String[] args) {
         String a = "abc", b = "cabcabca";
-        int res = new RepeatedStringMatchMath().repeatedStringMatch(a, b);
+        int res = new RepeatedStringMatchLength().repeatedStringMatch(a, b);
         System.out.println(res);
     }
 }
 
-class RepeatedStringMatchMath {
+class RepeatedStringMatchLength {
     public int repeatedStringMatch(String a, String b) {
         int n = (int) Math.ceil((double) b.length() / a.length());
         return a.repeat(n).contains(b) ? n : a.repeat(n + 1).contains(b) ? n + 1 : -1;
