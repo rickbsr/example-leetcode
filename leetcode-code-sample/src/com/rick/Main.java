e4a196047a6f16a64c2ad491bb37cae153b83f40
rickren <rickren@megabank.com.tw>
2022-09-07 13:18:03 +0800

    Update

--- leetcode-code-sample/src/com/rick/Main.java
+++ leetcode-code-sample/src/com/rick/Main.java
@@ -1,9 +1,12 @@
 package com.rick;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code her
+
     }
 }
 
--- leetcode-code-sample/src/com/rick/problems/easy/PlusOne.java
+++ leetcode-code-sample/src/com/rick/problems/easy/PlusOne.java
@@ -2,35 +2,20 @@ package com.rick.problems.easy;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.RoundingMode;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PlusOne {
 
     public static void main(String[] args) {
-        int[] digits = {6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3}, res;
-
-        res = new PlusOneOneByOnFromUnitsDigit().plusOne(digits);
+        int[] digits = {9, 9, 9, 9}, res;
 
         res = new PlusOneString().plusOne(digits);
-
+        res = new PlusOneMathSimple().plusOne(digits);
         res = new PlusOneMath().plusOne(digits);
+        res = new PlusOneOneByOnFromUnitsDigit().plusOne(digits);
 
         for (int i : res) System.out.print(i + " ");
-    }
-
-
-}
-
-class PlusOneOneByOnFromUnitsDigit {
-    public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--)
-            if (digits[i] != 9) {
-                digits[i]++;
-                return digits;
-            } else digits[i] = 0;
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
-        return digits;
     }
 }
 
@@ -40,18 +25,54 @@ class PlusOneString {
 
         StringBuilder builder = new StringBuilder();
 
+        // 第一階段：將「數列」轉換成「數值」
         for (int digit : digits) builder.append(digit);
 
-        String digitStr = new BigInteger(builder.toString()).add(BigInteger.ONE).toString();
+        int idx = lastIndexOfNotNineRegEx(builder.toString());
 
-        if (digitStr.length() == digits.length)
-            for (int i = 0; i < digits.length; i++)
-                digits[i] = Integer.parseInt(String.valueOf(digitStr.charAt(i)));
-        else {
+        // 第二階段：將「數值」轉換回「數列」
+        if (idx != -1) { // 若「位數不變」
+            for (int i = 0; i < digits.length; i++)  // 加「1」
+                if (i == idx) digits[i] = builder.charAt(i) - '0' + 1;
+                else if (i > idx) digits[i] = 0;
+        } else { // 全為「9」的情況，位數改變，須建立新數列
             digits = new int[digits.length + 1];
             digits[0] = 1;
         }
 
+        return digits;
+    }
+
+    private int lastIndexOfNotNineRegEx(String sourceStr) {
+        int idx = -1;
+        Matcher matcher = Pattern.compile("[0-8]").matcher(sourceStr);
+        while (matcher.find()) idx = matcher.start();
+        return idx;
+    }
+}
+
+class PlusOneMathSimple {
+
+    public int[] plusOne(int[] digits) {
+        int digit = 0;
+
+        // 第一階段：將「數列」轉換成「數值」
+        for (int i = 0; i < digits.length; i++)
+            digit += digits[i] * Math.pow(10, (digits.length - 1 - i));
+
+        // 加一
+        digit++;
+
+        // 第二階段：將「數值」轉換回「數列」
+        for (int i = 0; digit != 0; digit /= 10, i++) {
+            int idx = digits.length - 1 - i;
+            if (idx == -1) {
+                digits = new int[digits.length + 1];
+                digits[0] = 1;
+                break;
+            }
+            digits[idx] = digit % 10;
+        }
         return digits;
     }
 }
@@ -88,3 +109,15 @@ class PlusOneMath {
     }
 }
 
+class PlusOneOneByOnFromUnitsDigit {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--)
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            } else digits[i] = 0;
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
